e7af7313b7dd2568489ecaac09f9911eb81d5f97
brooke176 <97928286+brooke176@users.noreply.github.com>
2024-03-04 17:39:32 -0500

    Initial Commit

---
+++ vibe check beta/vibe_check_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibe_check_betaApp.swift
+//  vibe check beta
+//
+//  Created by Brooke Skinner on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct vibe_check_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
